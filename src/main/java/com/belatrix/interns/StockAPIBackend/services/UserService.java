26a9bc8cfba877f04d38040868a76c7347441faa
fer_g_balsas <fbalsas@belatrixsf.com>
2019-11-06 09:36:49 -0300

    Working on loggin process

---
+++ src/main/java/com/belatrix/interns/StockAPIBackend/services/UserService.java
@@ -0,0 +1,7 @@
+package com.belatrix.interns.StockAPIBackend.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class UserService implements UserDetailsService{
+
+}
